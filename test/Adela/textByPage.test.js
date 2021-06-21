b0866bf0b64f5d92e05f105f7b0a4179165cd735
Priscila Bey <pri.bey@gmail.com>
2021-06-21 16:59:05 -0300

    fix test for getbypage

--- test/Adela/textByPage.test.js
+++ test/Adela/textByPage.test.js
@@ -2,12 +2,24 @@ import createServer from "../../src/shared/server/server.js";
 import assert from "assert";
 import axios from "axios";
 
-const first9Fiction = {genre:"fiction", textsIds:[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-const first9NonFiction = {genre:"non_fiction", textsIds:[11, 12, 13, 14,15, 16, 17, 18]}
-const first9Poetry = {genre:"poetry", textsIds:[20, 21, 22, 23,24, 25, 26, 27]}
-const twoPagePoetry = {genre:"poetry", textsIds:[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+const first9Fiction = {
+  genre: "fiction",
+  textsIds: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+};
+const first9NonFiction = {
+  genre: "non_fiction",
+  textsIds: [11, 12, 13, 14, 15, 16, 17, 18],
+};
+const first9Poetry = {
+  genre: "poetry",
+  textsIds: [20, 21, 22, 23, 24, 25, 26, 27],
+};
+const twoPagePoetry = {
+  genre: "poetry",
+  textsIds: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+};
 
-const errorStatus = 403;
+const errorStatus = 400;
 
 let server;
 
@@ -25,10 +37,9 @@ describe("server", () => {
       const res = await axios.get(
         "http://localhost:8080/texts?page=1&genre=fiction"
       );
-      const textsIds = 
-        res.data.content
-        .map(text => text.idText)
-        .sort((a, b) =>  a - b);
+      const textsIds = res.data.content
+        .map((text) => text.idText)
+        .sort((a, b) => a - b);
 
       assert.deepStrictEqual(textsIds, first9Fiction.textsIds);
       assert.deepStrictEqual(res.data.content[0].genre, first9Fiction.genre);
@@ -38,10 +49,9 @@ describe("server", () => {
       const res = await axios.get(
         "http://localhost:8080/texts?page=1&genre=non_fiction"
       );
-      const textsIds = 
-        res.data.content
-        .map(text => text.idText)
-        .sort((a, b) =>  a - b);
+      const textsIds = res.data.content
+        .map((text) => text.idText)
+        .sort((a, b) => a - b);
 
       assert.deepStrictEqual(textsIds, first9NonFiction.textsIds);
       assert.deepStrictEqual(res.data.content[0].genre, first9NonFiction.genre);
@@ -51,10 +61,9 @@ describe("server", () => {
       const res = await axios.get(
         "http://localhost:8080/texts?page=1&genre=poetry"
       );
-      const textsIds = 
-        res.data.content
-        .map(text => text.idText)
-        .sort((a, b) =>  a - b);
+      const textsIds = res.data.content
+        .map((text) => text.idText)
+        .sort((a, b) => a - b);
 
       assert.deepStrictEqual(textsIds, first9Poetry.textsIds);
       assert.deepStrictEqual(res.data.content[0].genre, first9Poetry.genre);
