689d006af85800607d9e8efa7fb83c14cf787ba2
Alex Costa <alexunio@hotmail.com>
2021-06-21 22:40:09 -0300

    Fix error server requests

--- src/shared/server/server.js
+++ src/shared/server/server.js
@@ -7,6 +7,19 @@ async function createServer(port) {
   const app = express();
   app.use(express.json());
 
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
   app.use("/users", createUserRouter());
   app.use("/signup", createSignUpRouter());
   app.use("/texts", createTextRouter());
